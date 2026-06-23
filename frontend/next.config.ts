e7e7214b51bfbd8c4bd312bd9359210a7cd5b269
ayuxmhz <aayujmhz020@gmail.com>
2026-06-23 21:54:51 +0545

    frontend: implement dashboard, routing, context, and API integration

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -9,9 +9,23 @@ const nextConfig: NextConfig = {
         hostname: "images.unsplash.com",
         port: "",
         pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8089",
+        pathname: "/**",
       }
     ]
-  }
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:8089/api/:path*", // Proxy to Backend
+      },
+    ];
+  },
 };
 
 export default nextConfig;
